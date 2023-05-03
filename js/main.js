db25f496d24d81ad2c5d9dca42d8f1716ab6b680
rnc1610 <rodrigocastro1080@gmail.com>
2023-05-03 19:14:51 +0200

    tp1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-/*comentario multilinea
-//comentario de una linea
-let precio = 512;
-let nombre = "Televisor";
-console.log(precio);//aclarar para que sirve
-const fecha = "12/12/2022" // const no deja variar el valor (es una variable constante)
-*/
-function solicitarNombre(){
-    let nombreIngresado   = prompt("Ingresar nombre")
-    alert("El nombre ingresado es " + nombreIngresado)
-} 
+// Función para agregar un producto al carrito
+function agregarProducto(nombreProducto, precioProducto) {
+    let carrito = localStorage.getItem("carrito");
+    if (carrito === null) {
+      carrito = [];
+    } else {
+      carrito = JSON.parse(carrito);
+    }
+    let producto = {
+      nombre: nombreProducto,
+      precio: precioProducto
+    };
+    carrito.push(producto);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    alert("Producto agregado al carrito");
+  }
+  
+  // Función para calcular el total de la compra
+  function calcularTotal() {
+    let carrito = localStorage.getItem("carrito");
+    if (carrito !== null) {
+      carrito = JSON.parse(carrito);
+      let total = 0;
+      for (let i = 0; i < carrito.length; i++) {
+        total += carrito[i].precio;
+      }
+      return total;
+    } else {
+      return 0;
+    }
+  }
+  
+  // Ejemplo de uso de las funciones
+  agregarProducto("Producto 1", 10);
+  agregarProducto("Producto 2", 15);
+  console.log(calcularTotal());
