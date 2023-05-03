de0cd9ef1aed3accfadf080275c52ba0cb729c0b
rnc1610 <rodrigocastro1080@gmail.com>
2023-05-03 17:58:30 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-/*comentario multilinea*/
+/*comentario multilinea
 //comentario de una linea
 let precio = 512;
 let nombre = "Televisor";
 console.log(precio);//aclarar para que sirve
 const fecha = "12/12/2022" // const no deja variar el valor (es una variable constante)
-
-
+*/
+function solicitarNombre(){
+    let nombreIngresado   = prompt("Ingresar nombre")
+    alert("El nombre ingresado es " + nombreIngresado)
+} 
