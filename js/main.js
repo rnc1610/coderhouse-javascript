1e36f071ef53dac5b52e81a123788674ed156335
rnc1610 <rodrigocastro1080@gmail.com>
2023-05-29 14:42:10 +0200

    cambios carrito entregar 3

--- js/main.js
+++ js/main.js
@@ -1,164 +1,157 @@
-// Obtener referencias a los botones de agregar al carrito y vaciar carrito
-const agregarProductoBtns = document.querySelectorAll(".agregarProductoBtn");
-const vaciarCarritoBtn = document.getElementById("vaciarCarritoBtn");
+document.addEventListener('DOMContentLoaded', function() {
+  const agregarProductoBtns = document.querySelectorAll('.agregarProductoBtn');
+  const carritoItems = document.getElementById('carrito-items');
+  const carritoTotal = document.getElementById('carrito-total');
+  const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
-// Obtener referencia al contenedor del carrito
-const carritoItems = document.getElementById("carritoItems");
+  let carrito = [];
+  agregarProductoBtns.forEach(function(btn) {
+    btn.addEventListener('click', agregarProductoAlCarrito);
+  });
+  vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 
-// Función para obtener la fila del producto en el carrito
-function obtenerFilaProducto(producto) {
-  const filas = carritoItems.querySelectorAll("tr");
+  // Agregar un producto al carrito
+  function agregarProductoAlCarrito(event) {
+    const producto = event.target.dataset.producto;
+    const existeEnCarrito = carrito.find(item => item.producto === producto);
 
-  for (const fila of filas) {
-    const nombreProducto = fila.querySelector("td:nth-child(2)").textContent;
-    if (nombreProducto === producto.nombre) {
-      return fila;
+    if (existeEnCarrito) {
+      existeEnCarrito.cantidad++;
+    } else {
+      carrito.push({
+        producto: producto,
+        cantidad: 1
+      });
+    }
+    mostrarCarrito();
+  }
+  // Vaciar el carrito
+  function vaciarCarrito() {
+    carrito = [];
+    mostrarCarrito();
+  }
+  // Mostrar el carrito en la tabla
+  function mostrarCarrito() {
+    carritoItems.innerHTML = '';
+    carrito.forEach(function(item) {
+      const fila = document.createElement('tr');
+      const columnaImagen = document.createElement('td');
+      const imagen = document.createElement('img');
+      imagen.src = `assets/Fotos/${item.producto}.avif`;
+      imagen.alt = item.producto;
+      imagen.classList.add('imgprod');
+      columnaImagen.appendChild(imagen);
+      fila.appendChild(columnaImagen);
+
+      const columnaProducto = document.createElement('td');
+      columnaProducto.textContent = obtenerNombreProducto(item.producto);
+      fila.appendChild(columnaProducto);
+      const columnaPrecio = document.createElement('td');
+      columnaPrecio.textContent = obtenerPrecio(item.producto);
+      fila.appendChild(columnaPrecio);
+
+      const columnaCantidad = document.createElement('td');
+      const cantidad = document.createElement('span');
+      cantidad.textContent = item.cantidad;
+      columnaCantidad.appendChild(cantidad);
+      const botonEliminar = document.createElement('button');
+      botonEliminar.textContent = 'Eliminar producto';
+      botonEliminar.addEventListener('click', function() {
+        restarProducto(item.producto);
+      });
+      columnaCantidad.appendChild(botonEliminar);
+      fila.appendChild(columnaCantidad);
+      
+      const columnaSubtotal = document.createElement('td');
+      columnaSubtotal.textContent = obtenerSubtotal(item.producto, item.cantidad);
+      fila.appendChild(columnaSubtotal);
+
+      // Agregar la fila a la tabla
+      carritoItems.appendChild(fila);
+    });
+
+    // Actualizar el total del carrito
+    carritoTotal.textContent = calcularTotal();
+  }
+
+  // Restar un producto del carrito
+  function restarProducto(producto) {
+    const productoIndex = carrito.findIndex(item => item.producto === producto);
+
+    if (productoIndex !== -1) {
+      const cantidad = carrito[productoIndex].cantidad;
+
+      if (cantidad > 1) {
+        carrito[productoIndex].cantidad--;
+      } else {
+        carrito.splice(productoIndex, 1);
+      }
+
+      mostrarCarrito();
     }
   }
 
-  return null;
-}
+  // Nombre de un producto
+  function obtenerNombreProducto(producto) {
+    switch (producto) {
+      case 'producto1':
+        return 'Cajonera';
+      case 'producto2':
+        return 'Cama';
+      case 'producto3':
+        return 'Sofá';
+      case 'producto4':
+        return 'Silla';
+      case 'producto5':
+        return 'Mesa';
+      case 'producto6':
+        return 'Carrito Auxiliar';
+      default:
+        return 'Producto desconocido';
+    }
+  }
 
-// Función para añadir un producto al carrito
-function agregarProductoAlCarrito(producto) {
-  const filaExistente = obtenerFilaProducto(producto);
+  // Precio de un producto
+  function obtenerPrecio(producto) {
+    switch (producto) {
+      case 'producto1':
+        return '$100';
+      case 'producto2':
+        return '$150';
+      case 'producto3':
+        return '$200';
+      case 'producto4':
+        return '$80';
+      case 'producto5':
+        return '$120';
+      case 'producto6':
+        return '$250';
+      default:
+        return 'Precio no disponible';
+    }
+  }
 
-  if (filaExistente) {
-    // Si el producto ya está en el carrito, aumentar la cantidad y actualizar el subtotal
-    const cantidadColumna = filaExistente.querySelector("td:nth-child(4)");
-    const cantidad = parseInt(cantidadColumna.textContent);
-    cantidadColumna.textContent = cantidad + 1;
+  // Calcular el subtotal del producto en el carrito
+  function obtenerSubtotal(producto, cantidad) {
+    const precio = obtenerPrecio(producto);
+    if (precio !== 'Precio no disponible') {
+      const precioNum = Number(precio.substring(1));
+      const subtotal = precioNum * cantidad;
+      return `$${subtotal}`;
+    }
+    return 'Subtotal no disponible';
+  }
 
-    const precio = parseFloat(producto.precio);
-    const subtotal = parseFloat(filaExistente.querySelector("td:nth-child(5)").textContent.replace("$", ""));
-    const nuevoSubtotal = subtotal + precio;
-    filaExistente.querySelector("td:nth-child(5)").textContent = "$" + nuevoSubtotal.toFixed(2);
-  } else {
-    // Crear fila para el producto en el carrito
-    const fila = document.createElement("tr");
-
-    // Agregar imagen en miniatura del producto
-    const imagen = document.createElement("img");
-    imagen.src = producto.imagen;
-    imagen.alt = producto.nombre;
-    imagen.classList.add("producto-imagen");
-    const imagenColumna = document.createElement("td");
-    imagenColumna.appendChild(imagen);
-    fila.appendChild(imagenColumna);
-
-    // Agregar nombre del producto
-    const nombreColumna = document.createElement("td");
-    nombreColumna.textContent = producto.nombre;
-    fila.appendChild(nombreColumna);
-
-    // Agregar precio del producto
-    const precioColumna = document.createElement("td");
-    precioColumna.textContent = "$" + producto.precio;
-    precioColumna.classList.add("text-center");
-    fila.appendChild(precioColumna);
-
-    // Agregar cantidad del producto
-    const cantidadColumna = document.createElement("td");
-    cantidadColumna.textContent = "1";
-    cantidadColumna.classList.add("text-center");
-    fila.appendChild(cantidadColumna);
-
-    // Agregar subtotal del producto
-    const subtotalColumna = document.createElement("td");
-    subtotalColumna.textContent = "$" + producto.precio;
-    subtotalColumna.classList.add("text-center");
-    fila.appendChild(subtotalColumna);
-
-    // Agregar botón de eliminar
-    const eliminarColumna = document.createElement("td");
-    const eliminarBtn = document.createElement("button");
-    eliminarBtn.textContent = "Eliminar";
-    eliminarBtn.classList.add("eliminar-btn");
-    eliminarBtn.addEventListener("click", () => {
-      fila.remove();
-      actualizarTotal();
+  // Calcular el total del carrito
+  function calcularTotal() {
+    let total = 0;
+    carrito.forEach(function(item) {
+      const precio = obtenerPrecio(item.producto);
+      if (precio !== 'Precio no disponible') {
+        const precioNum = Number(precio.substring(1));
+        total += precioNum * item.cantidad;
+      }
     });
-    eliminarColumna.appendChild(eliminarBtn);
-    fila.appendChild(eliminarColumna);
-
-    // Agregar fila al carrito
-    carritoItems.appendChild(fila);
+    return `$${total}`;
   }
-
-  // Actualizar el total
-  actualizarTotal();
-}
-
-// Función para actualizar el total del carrito
-function actualizarTotal() {
-  const subtotalColumnas = document.querySelectorAll("#carritoItems td:nth-child(5)");
-  let total = 0;
-
-  subtotalColumnas.forEach((columna) => {
-    const subtotal = parseFloat(columna.textContent.replace("$", ""));
-    total += subtotal;
-  });
-
-  // Actualizar el contenido del contenedor del total
-  const totalContainer = document.getElementById("totalContainer");
-  totalContainer.textContent = "El total de su carrito es de $" + total.toFixed(2);
-}
-
-// Agregar listeners a los botones de agregar al carrito
-agregarProductoBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const productoId = btn.getAttribute("data-producto");
-    const producto = obtenerProducto(productoId);
-    agregarProductoAlCarrito(producto);
-  });
-});
-
-// Función para obtener los detalles del producto según su ID
-function obtenerProducto(id) {
-  switch (id) {
-    case "producto1":
-      return {
-        nombre: "Cajonera",
-        precio: 100,
-        imagen: "./assets/Fotos/producto1.avif",
-      };
-    case "producto2":
-      return {
-        nombre: "Cama",
-        precio: 150,
-        imagen: "./assets/Fotos/producto2.avif",
-      };
-    case "producto3":
-      return {
-        nombre: "Sofa",
-        precio: 200,
-        imagen: "./assets/Fotos/producto3.avif",
-      };
-    case "producto4":
-      return {
-        nombre: "Silla",
-        precio: 80,
-        imagen: "./assets/Fotos/producto4.avif",
-      };
-    case "producto5":
-      return {
-        nombre: "Mesa",
-        precio: 220,
-        imagen: "./assets/Fotos/producto5.avif",
-      };
-    case "producto6":
-      return {
-        nombre: "Carrito auxiliar",
-        precio: 50,
-        imagen: "./assets/Fotos/producto6.avif",
-      };
-    default:
-      return null;
-  }
-}
-
-// Vaciar el carrito al hacer clic en el botón "Vaciar carrito"
-vaciarCarritoBtn.addEventListener("click", () => {
-  carritoItems.innerHTML = "";
-  actualizarTotal();
 });
